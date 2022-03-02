e6b35c65406573614671d622290f428926914950
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-03-03 08:52:30 +0900

    Fix typo

--- tickets/billetterie_grpc.pb.go
+++ tickets/billetterie_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.19.4
-// source: tickets/billetrie.proto
+// source: tickets/billetterie.proto
 
 package routeguide
 
@@ -173,5 +173,5 @@ var Billetterie_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "tickets/billetrie.proto",
+	Metadata: "tickets/billetterie.proto",
 }
